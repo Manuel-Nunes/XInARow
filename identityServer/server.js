eadbbba3eba360eff20d485ae3e79ebc2dbd2e6e
Matt Kretschmer <matthewk@bbd.co.za>
2023-06-23 14:42:09 +0200

    testing sls deployment

--- identityServer/server.js
+++ identityServer/server.js
@@ -1,5 +1,5 @@
 console.log('Starting Identity Server');
-
+const serverless = require('serverless-http');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -8,27 +8,34 @@ const app = express();
 const serverFolder = 'identityServer';
 const { Config } = require('../globalUtils/configManager');
 
-const conf = new Config(`./${serverFolder}/serverConfig.json`);
-const PORT = conf.get('serverPort');
+// const conf = new Config(`./${serverFolder}/serverConfig.json`);
+// const PORT = conf.get('serverPort');
 
 console.log('Loading Static Folders');
-fs.readdirSync(`./${serverFolder}/public` ,{
-  withFileTypes: true 
-})
-  .filter(item => item.isDirectory())
-  .forEach(folder => {
-    app.use(express.static( path.join(__dirname,'public',folder.name)));
-  });
+// fs.readdirSync(`./${serverFolder}/public` ,{
+//   withFileTypes: true 
+// })
+//   .filter(item => item.isDirectory())
+//   .forEach(folder => {
+//     app.use(express.static( path.join(__dirname,'public',folder.name)));
+//   });
 
-app.get('/i', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index2.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port http://localhost:${PORT}`);
-});
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
+// app.get('/i', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/index2.html'));
 // });
 
+app.get('/', (req,res) => {
+  res.send('heys')
+})
+
+app.post('/login', (req,res) => {
+  
+})
+
+app.listen(3000, () => {
+  console.log(`App listening on port http://localhost:${3000}`);
+});
+
+module.exports.handler = serverless(app, {
+  binary: ['multipart/form-data'],
+});
