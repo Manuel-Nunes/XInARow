6090f16a062e8671f9edf694c1edb2c6c5821105
Matt Kretschmer <matthewk@bbd.co.za>
2023-06-23 18:06:47 +0200

    more stuff even more

--- identityServer/server.js
+++ identityServer/server.js
@@ -1,32 +1,17 @@
 console.log('Starting Identity Server');
 const serverless = require('serverless-http');
 const express = require('express');
-// const fs = require('fs');
-// const path = require('path');
+
 
 const app = express();
-// const serverFolder = 'identityServer';
-// const { Config } = require('../globalUtils/configManager');
 
-// const conf = new Config(`./${serverFolder}/serverConfig.json`);
-// const PORT = conf.get('serverPort');
 
 console.log('Loading Static Folders');
-// fs.readdirSync(`./${serverFolder}/public` ,{
-//   withFileTypes: true 
-// })
-//   .filter(item => item.isDirectory())
-//   .forEach(folder => {
-//     app.use(express.static( path.join(__dirname,'public',folder.name)));
-//   });
 
-// app.get('/i', function(req, res) {
-//   res.sendFile(path.join(__dirname, '/index2.html'));
-// });
 
 app.get('/', (req,res, callback) => {
-  // res.send('heys')
-  callback('heyyyy')
+
+  callback(null,{msg:'heyyyy'})
 })
 
 app.post('/login', (req,res) => {
