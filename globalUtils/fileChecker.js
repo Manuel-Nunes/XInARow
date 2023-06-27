3a2bcd7b020dc6e6a27e457096012db8a5d5e425
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-06-27 10:33:31 +0200

    Fixed file check Issue

--- globalUtils/fileChecker.js
+++ globalUtils/fileChecker.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 
 function checkForHTTPsFiles(){
   let hasAll = true;
-  if (fs.existsSync('../cert.pem'))
+  if (!fs.existsSync('./cert.pem'))
   {
     console.log('Missing Cert file');
     hasAll = false;
   }
 
-  if (fs.existsSync('../key.pem')) {
+  if (!fs.existsSync('./key.pem')) {
     console.log('Missing key file');
     hasAll = false;
   }
@@ -18,7 +18,7 @@ function checkForHTTPsFiles(){
 
 function checkSecrets(path) {
   let hasAll = true;
-  if (fs.existsSync(`../${path}/secrets.json`)) {
+  if (!fs.existsSync(`./${path}/secrets.json`)) {
     console.log('Missing Secrets file');
     hasAll = false;
   }
