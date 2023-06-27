5ae73b9540bc0e5b687a90d7451cbf88c747315c
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-06-27 10:11:40 +0200

    Fixed security Issue; Added file check;

---
+++ globalUtils/fileChecker.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function checkForHTTPsFiles(){
+  let hasAll = true;
+  if (fs.existsSync('../cert.pem'))
+  {
+    console.log('Missing Cert file');
+    hasAll = false;
+  }
+
+  if (fs.existsSync('../key.pem')) {
+    console.log('Missing key file');
+    hasAll = false;
+  }
+
+  return hasAll;
+}
+
+function checkSecrets(path) {
+  let hasAll = true;
+  if (fs.existsSync(`../${path}/secrets.json`)) {
+    console.log('Missing Secrets file');
+    hasAll = false;
+  }
+
+  return hasAll;
+}
+
+function comboCheck(path){
+  let hasAll = true;
+  if (!checkForHTTPsFiles())
+    hasAll = false;
+  if (!checkSecrets(path))
+    hasAll = false;
+
+  return hasAll;
+}
+
+module.exports = {
+  checkForHTTPsFiles,
+  checkSecrets,
+  comboCheck
+};
